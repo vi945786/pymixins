75fd36c420618ecee63c7c6c9595cf95b23c6faf
vi945786 <87494724+vi945786@users.noreply.github.com>
2025-04-09 19:29:23 +0300

    Add files via upload

---
+++ pymixins/object_manipulator.py
@@ -0,0 +1,68 @@
+import gc
+from typing import Any, Tuple, List
+
+
+def get_all_keys(obj: Any, do_attrs=True, skip_types=()) -> List[Tuple[Any, str]]:
+    keys = []
+    if do_attrs:
+        keys += [(attr, "attr") for attr in dir(obj)]
+
+    if not isinstance(obj, skip_types):
+        if isinstance(obj, (list, tuple)):
+            keys += [(i, "index") for i in range(len(obj))]
+        elif isinstance(obj, set):
+            keys += [(i, "old_val") for i in obj]
+        elif isinstance(obj, dict):
+            keys += [(k, "keys") for k in obj.keys()]
+    return keys
+
+
+def get_keys_from_value(obj: Any, value: Any, do_attrs=True, skip_types=()) -> List[Tuple[Any, str]]:
+    keys = []
+
+    if do_attrs:
+        val_id = id(value)
+        for attr in dir(obj):
+            if id(getattr(obj, attr)) == val_id:
+                keys.append((attr, "attr"))
+
+    if not isinstance(obj, skip_types):
+        if isinstance(obj, (list, tuple)):
+            keys.extend((i, "index") for i, v in enumerate(obj) if v is value)
+        elif isinstance(obj, set):
+            keys += [(i, "old_val") for i in obj]
+        elif isinstance(obj, dict):
+            keys.extend((k, "keys") for k, v in obj.items() if v is value)
+    return keys
+
+
+def get_value(obj: Any, ref: Tuple[Any, str]) -> Any:
+    if ref[1] == "attr":
+        return getattr(obj, ref[0])
+    elif ref[1] == "index" or ref[1] == "keys":
+        return obj[ref[0]]
+    elif ref[1] == "old_val":
+        return ref[0]
+
+
+def set_value(obj: Any, ref: Tuple[Any, str], value: Any) -> None:
+    try:
+        if ref[1] == "attr":
+            setattr(obj, ref[0], value)
+        elif ref[1] == "index" or ref[1] == "keys":
+            if isinstance(obj, tuple):
+                index = int(ref[0])
+                new_tuple = obj[:index] + (value,) + obj[index+1:]
+                for referrer in gc.get_referrers(obj):
+                    for key in get_keys_from_value(referrer, obj):
+                        set_value(referrer, key, new_tuple)
+            else:
+                obj[ref[0]] = value
+        elif ref[1] == "old_val":
+            obj.discard(ref[0])
+            obj.add(value)
+    except (AttributeError, TypeError) as e:
+        ok_exceptions = ["readonly attribute", "not writable", "not support item assignment"]
+        e_str = str(e)
+        if all([ok not in e_str for ok in ok_exceptions]):
+            raise
--- test.py
+++ test.py
@@ -1,33 +1,17 @@
-import mixin
-
-import modify
+import pymixins
 import time
-from modify import func
-from modify import func as get_status
-class Test: from modify import func
-func_ref = __import__("weakref").ref(func)
-func_tuple = (func,)
+import tests
 
-assert modify.func() == "original"
-assert func() == "original"
-assert get_status() == "original"
-assert Test.func() == "original"
-assert func_ref()() == "original"
-assert func_tuple[0]() == "original"
+tests.test("original")
 
-with open(mixin.get_module_file("modify")) as file:
+with open(pymixins.get_module_file("modify")) as file:
     code = file.readlines()
 code[1] = "    return 'modified'"
 time1 = time.time_ns()
-mixin.redefine_modules_file_as_code(("modify", "\n".join(code)))
+pymixins.redefine_modules_file_as_code(("modify", "\n".join(code)))
 time2 = time.time_ns()
 print(f"{(time2 - time1) / 1_000_000_000}s")
 
-assert modify.func() == "modified"
-assert func() == "modified"
-assert get_status() == "modified"
-assert Test.func() == "modified"
-assert func_ref()() == "modified"
-assert func_tuple[0]() == "modified"
+tests.test("modified")
 
 print("all tests passed")
